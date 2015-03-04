9d20b9611880a58dfa060cd2fad243f9d05116bc
Hwasung Lee <hwasung.lee@oriel.oxon.org>
2015-03-04 21:47:52 +0000

    Adding more pattern matching solutions

--- src/main/scala/info/technicallycorrect/fpscala/PatternMatching.scala
+++ src/main/scala/info/technicallycorrect/fpscala/PatternMatching.scala
@@ -10,4 +10,25 @@ object PatternMatching {
     case Nil => Nil
   }
 
+  def setHead[A](x: A, xs: List[A]): List[A] = xs match {
+    case _ :: tail => x :: tail
+    case Nil => List(x)
+  }
+
+  def drop[A](xs: List[A], n: Int): List[A] = xs match {
+    case _ :: tail => if (n > 0) drop(tail, n - 1) else xs
+    case Nil => Nil
+  }
+
+  def dropWhile[A](lst: List[A], f: A => Boolean): List[A] = lst match {
+    case l :: ls => if (f(l)) l :: dropWhile(ls, f) else dropWhile(ls, f)
+    case Nil => Nil
+  }
+
+  def init[A](lst: List[A]): List[A] = lst match {
+    case Nil => Nil
+    case l :: Nil => Nil
+    case l :: ls => l :: init(ls)
+  }
+
 }
--- src/test/scala/info/technicallycorrect/fpscala/PatternMatchingTest.scala
+++ src/test/scala/info/technicallycorrect/fpscala/PatternMatchingTest.scala
@@ -9,4 +9,27 @@ class PatternMatchingTest extends FlatSpec with Matchers {
     PatternMatching.tail(Nil) should be (Nil)
   }
 
+  "setHead" should "return a new list with the head element" in {
+    PatternMatching.setHead(4, List(1, 2, 3)) should be (List(4, 2, 3))
+    PatternMatching.setHead(4, Nil) should be (List(4))
+  }
+
+  "drop" should "drop the first n elements" in {
+    PatternMatching.drop(List(1, 2, 3), 0) should be (List(1, 2, 3))
+    PatternMatching.drop(List(1, 2, 3), 2) should be (List(3))
+    PatternMatching.drop(List(1, 2, 3), 3) should be (Nil)
+    PatternMatching.drop(List(1, 2, 3), 4) should be (Nil)
+  }
+
+  "dropWhile" should "drop the elements if predicate is satisfied" in {
+    val isOdd: Int => Boolean = x => x % 2 == 0
+    PatternMatching.dropWhile(List(1, 2, 3), isOdd) should be (List(2))
+  }
+
+  "init" should "is Clojure's butlast" in {
+    PatternMatching.init(Nil) should be (Nil)
+    PatternMatching.init(List(1)) should be (Nil)
+    PatternMatching.init(List(1, 2, 3)) should be (List(1, 2))
+  }
+
 }
